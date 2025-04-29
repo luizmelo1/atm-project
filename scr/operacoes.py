2bcc92b8df133445cba4487b31824fe961f92200
Luiz Felipe Melo <felipemeloyp@gmail.com>
2025-04-29 03:17:06 -0300

    começo do código de operações

--- scr/operacoes.py
+++ scr/operacoes.py
@@ -1,1 +1,18 @@
 #Saque, depósito, extrato
+from datetime import datetime
+from .database import carregar_usuarios, salvar_usuarios
+
+def consultar_saldo(conta: str) -> None:
+    """"Exibir o saldo atual da conta do usuário"""
+    print(f"\nSaldo atual: R$ {usuarios[conta]['saldo']:.2f}")
+
+    def sacar(conta: str) -> None:
+        """"Realizar operações de saque com validação"""
+        usuario = carregar_usuarios()
+
+        try:
+            valor = float(input('\nValor do saque: R$ '))
+
+            # Validações
+            if valor <= 0:
+                print('Erro: VAlor deve ser positivo.')
